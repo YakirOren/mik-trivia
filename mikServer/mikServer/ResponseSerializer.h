9ea7cfad2e4957daaef80af838aa76ae58f9259d
Yakir Oren <yakiroren@gmail.com>
2020-05-06 17:03:15 +0300

    add the ResponseSerializer class

---
+++ mikServer/mikServer/ResponseSerializer.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "json.hpp"
+#include <bitset>
+#include <string>
+
+using json = nlohmann::json;
+
+struct ErrorResponse
+{
+	std::string message;
+
+};
+
+struct LoginResponse
+{
+	unsigned int status;
+};
+
+struct SignupResponse
+{
+	unsigned int status;
+};
+
+class ResponseSerializer
+{
+public:
+	static std::string serializeResponse(ErrorResponse res);
+	static std::string serializeResponse(LoginResponse res);
+	static std::string serializeResponse(SignupResponse res);
+	static std::string to_bin(std::string str);
+
+};
+
