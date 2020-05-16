713f9386f3e6f6f3abf7485728e9676c3511ff29
Yakir Oren <yakiroren@gmail.com>
2020-05-16 16:24:29 +0300

    add fields and function signatures to the loginManager class.

--- mikServer/mikServer/LoginManager.h
+++ mikServer/mikServer/LoginManager.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include "LoggedUser.h"
+#include "IDatabase"
+
 class LoginManager
 {
+private:
+	IDatabase* m_database;
+	std::vector<LoggedUser> m_loggedUsers;
 
-
+public:
+	void signup(std::string name, std::string password, std::string email);
+	void login(std::string name, std::string password);
+	void logout(std::string name);
 
 
 };
