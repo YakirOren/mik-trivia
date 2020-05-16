fd71b8f24edbde174e3bb5176b73c7087f908f23
Yakir Oren <yakiroren@gmail.com>
2020-05-16 16:08:33 +0300

    basic files for the sql database implementation.

---
+++ mikServer/mikServer/sqlDatabase.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "IDatabase.h"
+#include "sqlite3.h"
+#include <string>
+
+class sqlDatabase :
+	public IDatabase
+{
+public:
+	sqlDatabase();
+	virtual ~sqlDatabase();
+
+	bool doesUserExists(int userId) override;
+	void createUser(std::string name, std::string password, std::string email) override;
+
+	bool isValidPassword(std::string) override;
+
+
+
+	bool open();
+	void close();
+
+
+private:
+
+	std::string dbName;
+	sqlite3* db;
+
+	int execute(std::string sql, int (*callback)(void*, int, char**, char**), void*, char** errmsg);
+	int execute(std::string sql);
+
+
+	// init functions.
+	static int int_callback(void* data, int argc, char** argv, char** azColName);
+
+};
+
