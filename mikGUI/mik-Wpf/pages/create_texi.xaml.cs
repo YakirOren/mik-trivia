9d40ec96ee9e0c83af4652ac7855aba3c0938c61
Yakir Oren <yakiroren@gmail.com>
2020-07-28 22:17:57 +0300

    add a background worker for seeking players in room update request codes add functions: startGameRequest getRoomState closeRoom leaveRoom

--- mikGUI/mik-Wpf/pages/create_texi.xaml.cs
+++ mikGUI/mik-Wpf/pages/create_texi.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using mik_Wpf.app_code;
 
@@ -13,12 +14,18 @@ namespace mik_Wpf
         public string AdminName;
         public int gameID;
         public MainWindow mainWindow;
-        public create_texi(int gameID, string AdminName,bool isAdmin = false)
+        private System.ComponentModel.BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+
+        public create_texi(int gameID, string AdminName, bool isAdmin = false)
         {
             this.gameID = gameID;
             this.isAdmin = isAdmin;
             this.AdminName = AdminName;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
             InitializeComponent();
+            InitializeBackgroundWorker();
 
             if (!this.isAdmin)
             {
@@ -27,11 +34,97 @@ namespace mik_Wpf
 
         }
 
-        private void exit_Click(object sender, RoutedEventArgs e)
+
+        // Set up the BackgroundWorker object by 
+        // attaching event handlers. 
+        private void InitializeBackgroundWorker()
         {
-            this.Close();
+            backgroundWorker1.DoWork +=
+                new DoWorkEventHandler(backgroundWorker1_DoWork);
+            backgroundWorker1.RunWorkerCompleted +=
+                new RunWorkerCompletedEventHandler(
+            backgroundWorker1_RunWorkerCompleted);
+
+            backgroundWorker1.ProgressChanged +=
+                new ProgressChangedEventHandler(
+            backgroundWorker1_ProgressChanged);
+        }
+
+
+        private void backgroundWorker1_ProgressChanged(object sender,
+            ProgressChangedEventArgs e)
+        {
+            players.Children.Clear();
+
+            var d = mainWindow.Client.GetPlayersInRoom(this.gameID);
+
+            driver_lbl.Text = d[0];
+
+            d.RemoveAt(0);
+
+            foreach (var name in d)
+            {
+                var new_game = new lobby_user(name);
+                players.Children.Add(new_game);
+                System.Console.WriteLine(name);
+
+            }
 
         }
+
+
+        // This event handler is where the actual,
+        // potentially time-consuming work is done.
+        private void backgroundWorker1_DoWork(object sender,
+            DoWorkEventArgs e)
+        {
+            // Get the BackgroundWorker that raised this event.
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            // Assign the result of the computation
+            // to the Result property of the DoWorkEventArgs
+            // object. This is will be available to the 
+            // RunWorkerCompleted eventhandler.
+
+            int d = 0;
+            while (!worker.CancellationPending)
+            {
+                d++;
+                worker.ReportProgress(d);
+                System.Threading.Thread.Sleep(3000);
+                d = 0;
+            }
+            e.Cancel = true;
+
+
+
+        }
+
+        // This event handler deals with the results of the
+        // background operation.
+        private void backgroundWorker1_RunWorkerCompleted(
+            object sender, RunWorkerCompletedEventArgs e)
+        {
+            // First, handle the case where an exception was thrown.
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                // Next, handle the case where the user canceled 
+                // the operation.
+                // Note that due to a race condition in 
+                // the DoWork event handler, the Cancelled
+                // flag may not have been set, even though
+                // CancelAsync was called.
+                System.Console.WriteLine("Canceled");
+            }
+
+
+        }
+
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -49,8 +142,11 @@ namespace mik_Wpf
             // when a new player joins add him to the list.
 
 
-            
-            getPlayers();
+            // Start the asynchronous operation.
+            backgroundWorker1.RunWorkerAsync();
+
+            //getPlayers();
+
             //mainWindow.Client.socket.ReceiveAll(); // wait for a messge from the server (the messge will be that a new client has join the room and this client should update the )
 
 
@@ -66,7 +162,7 @@ namespace mik_Wpf
             var d = mainWindow.Client.GetPlayersInRoom(this.gameID);
 
             driver_lbl.Text = d[0];
-            
+
             d.RemoveAt(0);
 
             foreach (var name in d)
@@ -88,10 +184,26 @@ namespace mik_Wpf
 
             // send start game request.
 
+            mainWindow.Client.startGameRequest(this.gameID);
+            backgroundWorker1.CancelAsync();
             _gameFrame.Navigate(new game(gameID));
 
 
         }
 
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (isAdmin)
+            {
+                mainWindow.Client.closeRoom(this.gameID);
+            }
+            else
+            {
+                mainWindow.Client.leaveRoom(this.gameID);
+            }
+
+            backgroundWorker1.CancelAsync();
+            //this.Close();
+        }
     }
 }
